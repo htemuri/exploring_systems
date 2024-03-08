4032a89d6f91a44cb9ec222b8909a837b52a49d2
htemuri <harris.temuri@gmail.com>
2024-03-07 23:59:48 -0500

    start book

---
+++ theme.config.jsx
@@ -0,0 +1,17 @@
+export default {
+    logo: <span>Exploring Systems</span>,
+    project: {
+        link: 'https://github.com/htemuri/exploring_systems'
+    },
+    docsRepositoryBase: 'https://github.com/htemuri/exploring_systems/',
+    sidebar: {
+        toggleButton: true
+    },
+    editLink: {
+        text: "Suggest an edit"
+    },
+    feedback: {
+        content: "Did I get something wrong? Give me feedback ->"
+    }
+    // ... other theme options
+}
